877c9c8c1be0e9c4ba1b6e6239823360eda32c54
Salman Khan <salmantechhub336@gmail.com>
2026-03-10 02:35:18 +0500

    add dart file for login screen

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:multi_roles_app/home_screen.dart';
+import 'package:multi_roles_app/login_screen.dart';
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
@@ -12,8 +13,8 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState(){
     super.initState();
 
-    Timer(Duration(seconds: 5), () {
-      Navigator.push(context, MaterialPageRoute(builder: (context)=> HomeScreen()));
+    Timer(Duration(seconds: 3), () {
+      Navigator.push(context, MaterialPageRoute(builder: (context)=> LoginScreen()));
     });
   }
 
