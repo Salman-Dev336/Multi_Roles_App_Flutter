2c28b49b0efec0e7ca9e4fca4375b98787895f93
Salman Khan <salmantechhub336@gmail.com>
2026-03-10 02:56:56 +0500

    make the button clickble by using inkwell function

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -11,20 +13,32 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-
-          Container(
-            height: 50,
-            width: double.infinity,
-            color: Colors.blue,
-            child: Center(
-              child: Text('Click here'),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InkWell(
+              onTap: () {
+                print('clicked');
+              },
+              child: Container(
+                height: 50,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.blue,
+                ),
+                child: const Center(
+                  child: Text(
+                    'Click here',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
