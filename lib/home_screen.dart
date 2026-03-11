8433864d187e039fb9e15ec702b7278ee642c703
Salman Khan <salmantechhub336@gmail.com>
2026-03-12 03:12:40 +0500

    beutify the homepage

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -10,8 +11,16 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('HomeScreen'),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        automaticallyImplyLeading: false,
+      ),
       body: SafeArea(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Center(
               child: Text('welcome to home screen',
@@ -21,9 +30,46 @@ class _HomeScreenState extends State<HomeScreen> {
                 color: Colors.deepOrange,
 
               ),
+            ),
+            ),
+            SizedBox(height: 40),
+            InkWell(
+              onTap: () async {
+
+                SharedPreferences sp = await SharedPreferences.getInstance();
+              
+
+
+                // sp.setString('name', 'salman');
+                // sp.setInt('age', 25);
+                // // print(sp.getString( 'name'));
+                // // print(sp.getInt('age').toString());  
+                // sp.remove('name');
+                // print(sp.getString( 'name'));
+                // sp.remove('age');
+                // print(sp.getInt('age').toString());
+                // // salman();
+                // print('clicked');
+                Navigator.push(context,
+                 MaterialPageRoute(builder: (context)=> HomeScreen()));
+              },
+              child: Container(
+                height: 50,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.blue,
+                ),
+                child: const Center(
+                  child: Text(
+                    'LogOut',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
               ),
+            ),
               //text
-              ),
+              
           ],
         ),
       ),
