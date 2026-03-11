5bece58031bed6c78337ea41eed1b0364bc75116
Salman Khan <salmantechhub336@gmail.com>
2026-03-12 03:17:44 +0500

    update

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -18,59 +18,64 @@ class _HomeScreenState extends State<HomeScreen> {
         automaticallyImplyLeading: false,
       ),
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Center(
-              child: Text('welcome to home screen',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.deepOrange,
-
-              ),
-            ),
-            ),
-            SizedBox(height: 40),
-            InkWell(
-              onTap: () async {
-
-                SharedPreferences sp = await SharedPreferences.getInstance();
-              
-
-
-                // sp.setString('name', 'salman');
-                // sp.setInt('age', 25);
-                // // print(sp.getString( 'name'));
-                // // print(sp.getInt('age').toString());  
-                // sp.remove('name');
-                // print(sp.getString( 'name'));
-                // sp.remove('age');
-                // print(sp.getInt('age').toString());
-                // // salman();
-                // print('clicked');
-                Navigator.push(context,
-                 MaterialPageRoute(builder: (context)=> HomeScreen()));
-              },
-              child: Container(
-                height: 50,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.blue,
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Text('welcome to home screen',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.green,
+          
                 ),
-                child: const Center(
-                  child: Text(
-                    'LogOut',
-                    style: TextStyle(color: Colors.white),
+              ),
+              ),
+              SizedBox(height: 40),
+              InkWell(
+                onTap: () async {
+          
+                  SharedPreferences sp = await SharedPreferences.getInstance();
+                
+          
+          
+                  // sp.setString('name', 'salman');
+                  // sp.setInt('age', 25);
+                  // // print(sp.getString( 'name'));
+                  // // print(sp.getInt('age').toString());  
+                  // sp.remove('name');
+                  // print(sp.getString( 'name'));
+                  // sp.remove('age');
+                  // print(sp.getInt('age').toString());
+                  // // salman();
+                  // print('clicked');
+                  Navigator.push(context,
+                   MaterialPageRoute(builder: (context)=> HomeScreen()));
+                },
+                child: Container(
+                  height: 50,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.blue,
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'LogOut',
+                      style: TextStyle(color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold),
+                    ),
                   ),
                 ),
               ),
-            ),
-              //text
-              
-          ],
+                //text
+                
+            ],
+          ),
         ),
       ),
     );
